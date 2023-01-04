b67f0a55673aa1b47a83182a4986a73a8c44f0de
Kristijonas92 <kristijonas.politika@gmail.com>
2023-01-04 16:02:10 +0200

    zinute

--- uzduotis/main.js
+++ uzduotis/main.js
@@ -1,9 +1,13 @@
-function toggleMenu() {
-    let menu = document.getElementById("sidebar-menu");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
-  
+let container = document.getElementById("#container");
+
+for (let i = 0; i < 5; i++) {
+let card = document.createElement("div");
+card.classList.add("card");
+card.innerHTML = `
+  <img src="https://blogen.influence4you.com/wp-content/uploads/2020/01/alexander-shatov-niUkImZcSP8-unsplash-1-scaled.jpg" alt="Video thumbnail">
+  <h3>Video title</h3>
+  <p>Video description</p>
+`;
+container.appendChild(card);
+
+}
